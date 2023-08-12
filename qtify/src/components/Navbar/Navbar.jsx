0811c2dad769704639ca95e22cd862aefbbbf48c
Kartik <kartiksparmar7@gmail.com>
2023-08-13 02:24:31 +0530

    Final Commit

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:qtify/src/components/Navbar/Navbar.jsx
 import React from 'react'
 import Button from '../Button/Button'
 import Logo from '../Logo/Logo'
@@ -15,22 +14,4 @@ const Navbar = () => {
   )
 }
 
-=======
-import React from 'react'
-import Button from '../Button/Button'
-import Logo from '../Logo/Logo'
-import styles from './Navbar.module.css'
-import Search from '../SearchBar/Search'
-
-const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-        <Logo />
-        <Search placeholder='Search an album of your choice'/>
-        <Button children='Give Feedback'/>
-    </nav>
-  )
-}
-
->>>>>>> L-square-QTify/main:src/components/Navbar/Navbar.jsx
 export default Navbar
--- qtify/src/components/SearchBar/Search.jsx
+++ qtify/src/components/SearchBar/Search.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:qtify/src/components/SearchBar/Search.jsx
 import React from 'react'
 import styles from "./Search.module.css"
 import {ReactComponent as SearchIcon} from '../../assets/SearchIcon.svg'
@@ -14,21 +13,4 @@ const Search = ({placeholder}) => {
   )
 }
 
-=======
-import React from 'react'
-import styles from "./Search.module.css"
-import {ReactComponent as SearchIcon} from '../../assets/SearchIcon.svg'
-
-const Search = ({placeholder}) => {
-  return (
-    <form className={styles.wrapper}>
-        <input className={styles.search} placeholder={placeholder}/>
-        <button className={styles.searchButton} type='submit'>
-            <SearchIcon />
-        </button>
-    </form>
-  )
-}
-
->>>>>>> L-square-QTify/main:src/components/SearchBar/Search.jsx
 export default Search
